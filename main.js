6a50433cf01f42bef1025485898c0618edd37ff7
Sthefany <sthefanyaraujopontes@gmail.com>
2022-05-08 08:47:17 -0300

    fist commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function displayAbout() {
+  var arrow = document.querySelector('#arrow');
+  arrow.classList.toggle('rotate');
+  
+  var about = document.querySelector('.about');
+  about.classList.toggle('active');
+}
